bb26dae221013e3318d17240dd2b4ad2c7e4cb1c
Sanjay Dutta <sanjoydutta1200@gmail.com>
2026-05-18 20:58:51 +0530

    fix: update Grow Site ID to U2l0ZToxNjdhNWMxOC02MWUxLTQxY2QtYTFlZC0zODVkNGUyNDFlM2M= statically in layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -169,7 +169,7 @@ export default async function RootLayout({
         <script
           data-grow-initializer=""
           dangerouslySetInnerHTML={{
-            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZToxNjdjNWMxOC02MWUxLTQxY2QtYTFIZC0zODVkNGUyNDFIM2M=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`
+            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZToxNjdhNWMxOC02MWUxLTQxY2QtYTFlZC0zODVkNGUyNDFlM2M=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`
           }}
         />
 
